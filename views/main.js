f1a2795a42754a1d10d6ad209b3b4cb287bb00dc
Vlad Balin <gaperton@gmail.com>
2010-11-11 19:22:12 +0800

    added templates test and description

--- views/main.js
+++ views/main.js
@@ -1,8 +1,26 @@
-$.include({
-	html : "main.html"
+$.include({ 		   // include...
+	html : "main.html" // ...html template for main page. 
+					   // Since html root is set in $.include.settings object, 
+					   // it will take the file from html/ dir.
 })	
-.define( function( _ ){
-	_.exports = function(){
-		return $( _.html.main() );
+.define( function( _ ){ // define module...
+	_.exports = function( $this ){ // ...which exports function, taking jQuery object as parameter...
+		_.html.renderTo( $this, {
+			tags : [
+				{ desc: 'Inline result of JS expression',
+				  mustache: '{{ expression }}',
+				  asp: '<%= expression %>' },
+				{ desc: 'Insert arbitrary JS code',
+				  mustache: '{- code; code; code; -}',
+				  asp: '<% code; code; code; %>' }
+			],
+		
+			sections : [
+			{ desc: 'Define template section',
+			  mustache: '{-- name --}',
+			  asp: '<%@ name %>' }			
+			],
+			block: '{-}', end: '{-}-}'
+		});// ...rendering template to the content of $this parameter.
 	}
 });
